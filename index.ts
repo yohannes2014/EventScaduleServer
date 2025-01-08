b8a72ea8e3aa4121ba7cc0f94b23b9e4ce66f3ad
jossysite <106884670+yohannes2014@users.noreply.github.com>
2025-01-08 17:16:17 +0300

    Set up initial folder structure with dependencies: TypeScript, dotenv, nodemon, and mongoose

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+
+
+
+
+const port = process.env.PORT;
+
+
+app.listen(port, ()=>{
+    console.log(`Running on port ${port}`)
+})
+
